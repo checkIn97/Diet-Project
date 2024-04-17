d98b9a149917788662fd0056a4ce40022a0715fc
donbacksu <ohdyeong@naver.com>
2024-04-17 22:12:52 +0900

    스프링 부트 초기 설정

---
+++ src/main/java/com/example/dietproject/DietProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.dietproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dietproject/DietProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dietproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
