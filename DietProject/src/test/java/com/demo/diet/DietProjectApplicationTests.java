771b8556a317f7d17c4f6a85162df697e48a9e41
checkin97 <tksdl1561@gmail.com>
2024-04-18 12:53:35 +0900

    \#5-User domain recreate

---
+++ DietProject/src/test/java/com/demo/diet/DietProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.diet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
