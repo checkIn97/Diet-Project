da5c1ddaf60b4856567a2378afab41d05ff8b29b
donbacksu <ohdyeong@naver.com>
2024-04-23 21:11:35 +0900

    #3 html 작업 mainpage.html 관리자페이지 말고 나머지 완  파일 경로 정리 css 로딩 안됨 해결

---
+++ FoodNavi/src/main/java/com/demo/controller/UsersController.java
@@ -0,0 +1,69 @@
+package com.demo.controller;
+
+import com.demo.domain.Users;
+import com.demo.persistence.UsersRepository;
+import com.demo.service.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UsersController {
+	@Autowired
+	private UsersService usersService;
+	@Autowired
+	private UsersRepository usersRepo;
+
+	@PostMapping("/join")
+	public String joinAction(Users vo, Model model) {
+		if (usersService.compareID(vo.getUserid()) == 0) {
+			model.addAttribute("msg", "이미 존재하는 아이디 입니다.");
+            return "alertPage";
+		} else {
+			Users user = Users.builder().userid(vo.getUserid()).userpw(vo.getUserpw()).name(vo.getName()).sex(vo.getSex())
+					.build();
+			usersService.insertUser(user);
+			model.addAttribute("useq", user.getUseq());
+		}
+		return "redirect:#bmi";
+	}
+
+
+	 @PostMapping("/insertBMI") 
+	 public String insertBMI(Users vo) { 
+		 Users user = usersService.getUserByMaxUseq(); 
+		 user.setAge(vo.getAge());
+		 user.setHeight(vo.getHeight()); 
+		 user.setWeight(vo.getWeight());
+		 usersService.insertUser(user); 
+		 return "redirect:#login"; 
+	  }
+	
+
+	@GetMapping("/login_form")
+	public String loginView() {
+		return "login";
+	}
+
+	@PostMapping("/login")
+	public String loginAction(Users vo, Model model) {
+		int useq = usersRepo.findByUserid(vo.getUserid()).get().getUseq();
+		String url = "";
+		if (usersService.loginID(vo) == 1) { // 정상 사용자
+			model.addAttribute("loginUser", usersService.getUser(useq));
+
+			url = "mainpage";
+		} else {
+			url = "login_fail";
+		}
+
+		return url;
+	}
+
+	@GetMapping("/contract")
+	public String contractView() {
+		return "contract";
+	}
+}
---
+++ FoodNavi/src/main/java/com/demo/service/UsersServiceImpl.java
@@ -0,0 +1,71 @@
+package com.demo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.demo.domain.Users;
+import com.demo.persistence.UsersRepository;
+
+@Service
+public class UsersServiceImpl implements UsersService {
+	
+	@Autowired
+	private UsersRepository usersRepo;
+	
+	@Override
+	public void insertUser(Users vo) {
+		// 회원 정보 저장
+		usersRepo.save(vo);
+	}
+
+	@Override
+	public int loginID(Users vo) {
+		int result = -1;
+		int useq = usersRepo.findByUserid(vo.getUserid()).get().getUseq();
+		System.out.println(useq);
+		
+		// Users 테이블에서 사용자 조회
+		Optional<Users> user = usersRepo.findById(useq);
+	
+		// 결과값 설정 :
+		// 1: ID,PWD 일치, 0: 비밀번호 불일치, -1: ID가 존재하지 않음.
+		if(user.isEmpty()) {
+			result = -1;
+		} else if(user.get().getUserpw().equals(vo.getUserpw())) {
+			result = 1;
+		} else {
+			result = 0; //비밀번호 불일치
+		}
+		return result;
+	}
+
+	@Override
+	public Users getUser(int useq) {
+		
+		return usersRepo.findById(useq).get();
+	}
+
+	@Override
+	public Users getUserByMaxUseq() {
+		return usersRepo.findFirstByOrderByUseqDesc();
+		
+	}
+
+	@Override
+	public int compareID(String id) {
+		int result = -1;
+		Optional<Users> user = usersRepo.findByUserid(id);
+		if (user.isEmpty()) {
+			result = -1;
+		} else if (user.get().getUserid().equals(id)) {
+			result = 0;
+		} else {
+			result = 1;
+		}
+		
+		return result;
+	}
+	
+}
