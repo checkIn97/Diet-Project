3f0048a42a94ac88318fc8e6c3fe97c3ce49e694
checkin97 <tksdl1561@gmail.com>
2024-04-25 15:54:18 +0900

    .#8 (1)회원가입: 아이디 영문자, 숫자 합쳐 6자 이상 / 비밀번호 영어대문자, 소문자, 숫자, 특수문자 각 1개 이상 합쳐 8자 이상
    (2)마이페이지 기능 구현 : 이름과 아이디는 수정 불가능

--- FoodNavi/src/main/java/com/demo/controller/UsersController.java
+++ FoodNavi/src/main/java/com/demo/controller/UsersController.java
@@ -1,5 +1,7 @@
 package com.demo.controller;
 
+import java.util.regex.Pattern;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,9 +25,22 @@ public class UsersController {
 
 	@PostMapping("/join")
 	public String joinAction(Users vo, Model model) {
+		String PATTERN_ID = "^[a-z]{1}[a-z0-9]{5,10}+$";
+		String PATTERN_PW = "^(?=.*[a-zA-Z])((?=.*\\d)|(?=.*\\W)).{8,128}+$";
+		boolean idPattern = Pattern.matches(PATTERN_ID, vo.getUserid());
+		boolean pwPattern = Pattern.matches(PATTERN_PW, vo.getUserpw());
 		if (usersService.compareID(vo.getUserid()) == 0) {
 			model.addAttribute("msg", "이미 존재하는 아이디 입니다.");
-            return "alertPage";
+            return "alertPage"; 
+		} else if (vo.getUserid() == null) {
+			model.addAttribute("msg", "아이디를 입력해주세요.");
+			return "alertPage";
+		} else if (!idPattern) {
+			model.addAttribute("msg", "아이디는 영문, 숫자 포함 6자리 이상 입력하셔야 합니다.");
+            return "alertPage"; 
+		} else if (!pwPattern) {
+			model.addAttribute("msg", "비밀번호는 영문 소문자, 대문자, 숫자, 특수문자를 반드시 하나씩 포함하여 8자리 이상 입력하셔야 합니다.");
+            return "alertPage"; 
 		} else {
 			Users user = Users.builder().userid(vo.getUserid()).userpw(vo.getUserpw()).name(vo.getName()).sex(vo.getSex())
 					.build();
@@ -68,7 +83,28 @@ public class UsersController {
 
 		return url;
 	}
-
+	
+	@PostMapping("/change_weight")
+	public String changeWeight(HttpSession session, Users vo, Model model) {
+		Users user = (Users)session.getAttribute("loginUser");
+		user.setWeight(vo.getWeight());
+		usersRepo.save(user);
+		model.addAttribute("msg", "체중 수정이 완료되었습니다.");
+		return "changeResult";
+	}
+	
+	@PostMapping("/update_user")
+	public String update_user(HttpSession session, Users vo, Model model) {
+		Users user = (Users)session.getAttribute("loginUser");
+		user.setUserpw(vo.getUserpw());
+		user.setAge(vo.getAge());
+		user.setHeight(vo.getHeight());
+		user.setWeight(vo.getWeight());
+		usersRepo.save(user);
+		model.addAttribute("msg", "회원정보 수정이 완료되었습니다.");
+		return "updateResult";
+	}
+	
 	@GetMapping("/contract")
 	public String contractView() {
 		return "contract";
