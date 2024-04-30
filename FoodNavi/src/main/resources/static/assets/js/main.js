3e90fad897c98e0066401b4f352f95de8db5909b
donbacksu <ohdyeong@naver.com>
2024-04-30 14:16:41 +0900

    #3 식단추천 토글버튼 활성화 main.js 와 main.css 토글버튼 관련된 항목 추가

--- FoodNavi/src/main/resources/static/assets/js/main.js
+++ FoodNavi/src/main/resources/static/assets/js/main.js
@@ -431,3 +431,9 @@ window.onload = function() {
         img.style.display = 'none';
     }
 }
+
+// 토글 버튼의 초기 상태를 설정합니다. 이 예제에서는 버튼이 'off' 상태로 시작합니다.
+let isButtonOn = false;
+
+// 토글 버튼 요소를 찾습니다. 이 예제에서는 버튼의 id가 'toggleButton'이라고 가정합니다.
+const toggleButton = document.getElementById('toggleButton');
