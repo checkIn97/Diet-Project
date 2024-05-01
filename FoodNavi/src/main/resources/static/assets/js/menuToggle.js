015022a4a1bc32edb71322a8f179e8f79589eb92
checkIn97 <158795073+checkIn97@users.noreply.github.com>
2024-05-01 10:52:05 +0900

    Delete FoodNavi/src/main/resources/static/assets directory

--- FoodNavi/src/main/resources/static/assets/js/menuToggle.js
+++
@@ -1,7 +0,0 @@
-function scrollToMembership() {
-    var membershipArticle = document.getElementById("membership");
-    if (membershipArticle) {
-        // 회원가입 섹션으로 부드럽게 스크롤
-        membershipArticle.scrollIntoView({ behavior: 'smooth' });
-    }
-}
