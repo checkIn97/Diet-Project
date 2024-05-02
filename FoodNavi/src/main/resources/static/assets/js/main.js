99722315dd6d918fbb34be3da4aceee5902437c5
checkIn97 <158795073+checkIn97@users.noreply.github.com>
2024-05-02 12:31:31 +0900

    Add files via upload

--- FoodNavi/src/main/resources/static/assets/js/main.js
+++ FoodNavi/src/main/resources/static/assets/js/main.js
@@ -431,3 +431,13 @@ window.onload = function() {
         img.style.display = 'none';
     }
 }
+
+// 토글 버튼의 초기 상태를 설정합니다. 'off' 상태로 시작합니다.
+let isButtonOn = false;
+
+// 토글 버튼 요소를 찾습니다.
+const toggleButton = document.getElementById('toggleButton');
+
+
+// 테이블 요소를 가져옵니다
+var ingredientsTable = document.getElementById('ingredientsTable');
