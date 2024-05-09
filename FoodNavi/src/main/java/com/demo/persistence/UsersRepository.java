39daa2b8b5c55dcc1e43fb7c416c1a6d38469350
checkIn97 <158795073+checkIn97@users.noreply.github.com>
2024-05-09 21:16:26 +0900

    Add files via upload

--- FoodNavi/src/main/java/com/demo/persistence/UsersRepository.java
+++ FoodNavi/src/main/java/com/demo/persistence/UsersRepository.java
@@ -1,10 +1,8 @@
 package com.demo.persistence;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.demo.domain.Users;
 
@@ -12,7 +10,4 @@ public interface UsersRepository extends JpaRepository<Users, Integer>{
 	public Optional<Users> findByUserid(String id);
 	
 	public Users findFirstByOrderByUseqDesc();
-	
-	@Query(value="SELECT * FROM users WHERE useyn=?1", nativeQuery=true)
-	public List<Users> getUserListByUseyn(String useyn);
 }
