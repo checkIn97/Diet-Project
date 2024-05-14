62cebef074773f2a0e693d32aad12d7c20e1653e
donbacksu <ohdyeong@naver.com>
2024-05-14 11:30:50 +0900

    #3 데이터에서 값을 받아오는 부분 추가

--- FoodNavi/src/main/java/com/demo/persistence/ExerciseRepository.java
+++ FoodNavi/src/main/java/com/demo/persistence/ExerciseRepository.java
@@ -3,7 +3,14 @@ package com.demo.persistence;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.demo.domain.Exercise;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.sql.Timestamp;
+import java.util.List;
 
 public interface ExerciseRepository extends JpaRepository<Exercise, Integer> {
 
+    @Query("SELECT e FROM Exercise e WHERE e.exerciseDate >= :startDate")
+    List<Exercise> findLastWeekExercises(@Param("startDate") Timestamp startDate);
 }
--- FoodNavi/src/main/resources/static/assets/js/activityChart.js
+++ FoodNavi/src/main/resources/static/assets/js/activityChart.js
@@ -2,16 +2,20 @@ Chart.defaults.color = '#eee'; // 전역으로 글씨 색상을 흰색으로 변
 
 var ctx = document.getElementById('myChart').getContext('2d');
 
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['일', '월', '화', '수', '목', '금', '토'],
-        datasets: [{
-            label: '운동시간',
-            data: [12, 19, 3, 5, 2, 3, 7],
-            backgroundColor: 'rgba(75, 192, 192, 0.2)',
-            borderColor: 'rgba(75, 192, 192, 1)',
-            borderWidth: 1
-        }]
-    },
-});
+fetch('/activities/data/last-week')
+    .then(response => response.json())
+    .then(data => {
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['일', '월', '화', '수', '목', '금', '토'],
+                datasets: [{
+                    label: '운동시간',
+                    data: data, // 운동 시간을 데이터로 사용
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 1
+                }]
+            },
+        });
+    });
