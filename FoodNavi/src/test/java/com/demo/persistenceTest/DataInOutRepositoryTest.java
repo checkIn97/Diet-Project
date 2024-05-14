f95daed7789f9b5e0319017f05f15fb1222aa108
donbacksu <ohdyeong@naver.com>
2024-05-14 14:59:53 +0900

    @Disabled가 안되어있던 부분 추가 주석 추가

--- FoodNavi/src/test/java/com/demo/persistenceTest/DataInOutRepositoryTest.java
+++ FoodNavi/src/test/java/com/demo/persistenceTest/DataInOutRepositoryTest.java
@@ -16,69 +16,68 @@ import com.demo.service.HistoryService;
 
 @SpringBootTest
 public class DataInOutRepositoryTest {
-	@Autowired
-	DataInOutService dataInOutService;
-	
-	@Autowired
-	HistoryService historyService;
-	
-	@Disabled
-	@Test
-	public void usersInDummy() {
-		// 입력할 더미 유저의 숫자
-		String csvFile = "tmp_users.csv";
-		String pyFile = "UsersListToCsv.py";
-		String n = "1000";
-		List<Users> usersList = dataInOutService.usersInDummy(n);
-		dataInOutService.usersListToCsv(usersList);
-	}
-	
-	@Disabled
-	@Test
-	public void ingredientInFromCsv() {
-		String csvFile = "igrd.csv";
-		String pyFile = "";
-		String n = "all";
-		List<Ingredient> ingredientList = dataInOutService.ingredientInFromCsv(csvFile, n);
-	}
-		
-	
-	@Disabled
-	@Test
-	public void foodInDummy() {
-		// csv 파일에서 food와 foodDetail 입력
-		String csvFile = "tmp_food.csv";
-		String pyFile = "FoodListToCsv.py";
-		String n = "100";
-		List<Food> foodList = dataInOutService.foodInDummy(n);
-		dataInOutService.foodListToCsv(foodList);
-	}
-	
+    @Autowired
+    DataInOutService dataInOutService;
 
-	@Test
-	public void foodInFromCsv() {
-		// csv 파일에서 food와 foodDetail 입력
-		String csvFile = "FoodIngredient.csv";
-		String pyFile = "";
-		String n = "all";		
-		List<Food> foodList = dataInOutService.foodInFromCsv(csvFile, n);
-		
-		
-	}
-	
-	
-	@Disabled
-	@Test
-	public void historyInDummy() {
-		String csvFile = "tmp_history.csv";
-		String pyFile = "HistoryListToCsv.py";
-		String n = "1000";
-		List<History> historyList = dataInOutService.historyInDummy(n);
-		dataInOutService.historyListToCsv(historyList);
-	}
-	
-	
-	
-	
-	
+    @Autowired
+    HistoryService historyService;
+
+    @Disabled
+    @Test
+    public void usersInDummy() {
+        // 입력할 더미 유저의 숫자
+        String csvFile = "tmp_users.csv";
+        String pyFile = "UsersListToCsv.py";
+        String n = "1000";
+        List<Users> usersList = dataInOutService.usersInDummy(n);
+        dataInOutService.usersListToCsv(usersList);
+    }
+
+//    재료 추가 test
+    @Disabled
+    @Test
+    public void ingredientInFromCsv() {
+        String csvFile = "igrd.csv";
+        String pyFile = "";
+        String n = "all";
+        List<Ingredient> ingredientList = dataInOutService.ingredientInFromCsv(csvFile, n);
+    }
+
+
+    @Disabled
+    @Test
+    public void foodInDummy() {
+        // csv 파일에서 food와 foodDetail 입력
+        String csvFile = "tmp_food.csv";
+        String pyFile = "FoodListToCsv.py";
+        String n = "100";
+        List<Food> foodList = dataInOutService.foodInDummy(n);
+        dataInOutService.foodListToCsv(foodList);
+    }
+
+
+    @Disabled
+    @Test
+    public void foodInFromCsv() {
+        // csv 파일에서 food와 foodDetail 입력
+        String csvFile = "FoodIngredient.csv";
+        String pyFile = "";
+        String n = "all";
+        List<Food> foodList = dataInOutService.foodInFromCsv(csvFile, n);
+
+
+    }
+
+
+    @Disabled
+    @Test
+    public void historyInDummy() {
+        String csvFile = "tmp_history.csv";
+        String pyFile = "HistoryListToCsv.py";
+        String n = "1000";
+        List<History> historyList = dataInOutService.historyInDummy(n);
+        dataInOutService.historyListToCsv(historyList);
+    }
+
+
 }
