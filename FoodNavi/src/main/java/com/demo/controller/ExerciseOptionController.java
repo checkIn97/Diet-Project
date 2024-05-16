cb460e199bb66201677c786dd0234029f0cf86fd
checkin97 <tksdl1561@gmail.com>
2024-05-16 16:42:25 +0900

    .#8 - 나의 활동 기록하기 기능 연결

--- FoodNavi/src/main/java/com/demo/controller/ExerciseOptionController.java
+++ FoodNavi/src/main/java/com/demo/controller/ExerciseOptionController.java
@@ -1,21 +1,34 @@
 package com.demo.controller;
 
-import com.demo.domain.ExerciseOption;
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
-import com.demo.persistence.ExerciseOptionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import com.demo.domain.Exercise;
+import com.demo.domain.ExerciseOption;
+import com.demo.domain.Users;
+import com.demo.persistence.ExerciseOptionRepository;
+import com.demo.persistence.ExerciseRepository;
+
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 
 @RestController
 public class ExerciseOptionController {
 
     @Autowired
     private ExerciseOptionRepository exerciseOptionRepository;
+    @Autowired
+    private ExerciseRepository exerciseRepo;
 
     @GetMapping("/activities/search")
     public List<String> searchIngredients(@RequestParam("term") String term) {
@@ -24,5 +37,26 @@ public class ExerciseOptionController {
                 .map(ExerciseOption::getType)
                 .collect(Collectors.toList());
     }
-
+    
+    @PostMapping("/exercise_record")
+    public void insertRecord(@RequestParam("activityType") String activityType,
+    						 @RequestParam("activityTime") String activityTime,
+    						 HttpSession session, HttpServletResponse response) throws IOException {
+    	Users user = (Users)session.getAttribute("loginUser");
+    	Optional<ExerciseOption> exo = exerciseOptionRepository.findByType(activityType);
+    	if (!exo.isEmpty()) {
+	    	Exercise vo = Exercise.builder()
+	    			.exerciseDate(new Date())
+	    			.exerciseOption(exo.get())
+	    			.exerciseType(activityType)
+	    			.time(Integer.parseInt(activityTime))
+	    			.user(user)
+	    			.build();
+	    	exerciseRepo.save(vo);
+    	} else {
+    		System.out.println("없는 운동");
+    	}
+    	String redirect_uri="http://localhost:8080/user_myactivity_view";
+    	response.sendRedirect(redirect_uri);
+    }
 }
--- FoodNavi/src/main/java/com/demo/domain/ExerciseOption.java
+++ FoodNavi/src/main/java/com/demo/domain/ExerciseOption.java
@@ -4,12 +4,9 @@ import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -34,8 +31,4 @@ public class ExerciseOption {
 	
 	private String type; // 운동이름
 	private String fomula; // 운동별 계산식
-	
-	@OneToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name= "exercise_eseq", referencedColumnName="eseq")
-	private Exercise exercise;
 }
