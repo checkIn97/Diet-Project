28a06b0bf69ffd27c44be57ad0042a517eb654e2
checkin97 <tksdl1561@gmail.com>
2024-05-27 15:32:58 +0900

    .#10 - 운동 기록에 따라 메인화면에서 표시되는 칼로리 수정

--- FoodNavi/src/main/java/com/demo/controller/HomeController.java
+++ FoodNavi/src/main/java/com/demo/controller/HomeController.java
@@ -1,8 +1,16 @@
 package com.demo.controller;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.demo.domain.Exercise;
+import com.demo.domain.ExerciseOption;
+import com.demo.persistence.ExerciseOptionRepository;
+import com.demo.service.ExerciseOptionService;
+import com.demo.service.ExerciseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,53 +26,71 @@ import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
-	
-	@Autowired
-	private HistoryService historyService;
-		
-	@GetMapping("/")
-	public String intropage() {
-		return "index";
-	}
-	
-	@GetMapping("/mainpage")
-	public String mainpage(HttpSession session, Model model) {
-		// 세션에서 사용자 정보 가져오기
-    	Users user = (Users) session.getAttribute("loginUser");
-    	// 세션에 로그인 정보가 없는 경우
+
+    @Autowired
+    private HistoryService historyService;
+
+    @Autowired
+    private ExerciseOptionRepository exerciseOptionRepo;
+
+    @Autowired
+    private ExerciseService exerciseService;
+
+    @GetMapping("/")
+    public String intropage() {
+        return "index";
+    }
+
+    @GetMapping("/mainpage")
+    public String mainpage(HttpSession session, Model model) {
+        // 세션에서 사용자 정보 가져오기
+        Users user = (Users) session.getAttribute("loginUser");
+        // 세션에 로그인 정보가 없는 경우
         if (user == null) {
             return "redirect:user_login_form"; // 로그인 페이지로 이동.
-        }	
-		
-		List<Board> boardList = new ArrayList<Board>();
+        }
+        int minusKcal = 0;
 
-		model.addAttribute("boardList", boardList);
-		UserVo userVo = new UserVo((Users)(session.getAttribute("loginUser")));
-		userVo.setKcalToday((int)historyService.totalKcalToday(userVo.getUser()));
-		userVo.setKcalOnTable((int)historyService.totalKcalOnTable(userVo.getUser()));
-		userVo.setCarbToday(Math.round(historyService.totalCarbToday(userVo.getUser())*100)/100f);
-		userVo.setCarbOnTable(Math.round(historyService.totalCarbOnTable(userVo.getUser())*100)/100f);
-		userVo.setPrtToday(Math.round(historyService.totalPrtToday(userVo.getUser())*100)/100f);
-		userVo.setPrtOnTable(Math.round(historyService.totalPrtOnTable(userVo.getUser())*100)/100f);
-		userVo.setFatToday(Math.round(historyService.totalFatToday(userVo.getUser())*100)/100f);
-		userVo.setFatOnTable(Math.round(historyService.totalFatOnTable(userVo.getUser())*100)/100f);
-		
-		model.addAttribute("userVo", userVo);
-		return "mainpage";
-	}
-	
-	@GetMapping("load_userVo")
-	@ResponseBody
-	public UserVo loadUserVo(HttpSession session) {
-		UserVo userVo = new UserVo((Users)(session.getAttribute("loginUser")));
-		userVo.setKcalToday((int)historyService.totalKcalToday(userVo.getUser()));
-		userVo.setKcalOnTable((int)historyService.totalKcalOnTable(userVo.getUser()));
-		userVo.setCarbToday(Math.round(historyService.totalCarbToday(userVo.getUser())*100)/100f);
-		userVo.setCarbOnTable(Math.round(historyService.totalCarbOnTable(userVo.getUser())*100)/100f);
-		userVo.setPrtToday(Math.round(historyService.totalPrtToday(userVo.getUser())*100)/100f);
-		userVo.setPrtOnTable(Math.round(historyService.totalPrtOnTable(userVo.getUser())*100)/100f);
-		userVo.setFatToday(Math.round(historyService.totalFatToday(userVo.getUser())*100)/100f);
-		userVo.setFatOnTable(Math.round(historyService.totalFatOnTable(userVo.getUser())*100)/100f);
-		return userVo;
-	}
+        List<Exercise> exList = exerciseService.getExerciseListByUseqAndType(user);
+        for (Exercise ex : exList) {
+            LocalDate today = LocalDate.now();
+            Instant instant = ex.getExerciseDate().toInstant();
+            LocalDate exLocal = instant.atZone(ZoneId.systemDefault()).toLocalDate();
+            if (exLocal.equals(today)) {
+                minusKcal += ((Integer.parseInt(ex.getExerciseOption().getFomula()) / 60) * ex.getTime());
+            }
+        }
+
+
+        List<Board> boardList = new ArrayList<Board>();
+
+        model.addAttribute("boardList", boardList);
+        UserVo userVo = new UserVo((Users) (session.getAttribute("loginUser")));
+        userVo.setKcalToday(((int) historyService.totalKcalToday(userVo.getUser()) - minusKcal) <= 0 ? 0 : ((int) historyService.totalKcalToday(userVo.getUser()) - minusKcal));
+        userVo.setKcalOnTable((int) historyService.totalKcalOnTable(userVo.getUser()));
+        userVo.setCarbToday(Math.round(historyService.totalCarbToday(userVo.getUser()) * 100) / 100f);
+        userVo.setCarbOnTable(Math.round(historyService.totalCarbOnTable(userVo.getUser()) * 100) / 100f);
+        userVo.setPrtToday(Math.round(historyService.totalPrtToday(userVo.getUser()) * 100) / 100f);
+        userVo.setPrtOnTable(Math.round(historyService.totalPrtOnTable(userVo.getUser()) * 100) / 100f);
+        userVo.setFatToday(Math.round(historyService.totalFatToday(userVo.getUser()) * 100) / 100f);
+        userVo.setFatOnTable(Math.round(historyService.totalFatOnTable(userVo.getUser()) * 100) / 100f);
+
+        model.addAttribute("userVo", userVo);
+        return "mainpage";
+    }
+
+    @GetMapping("load_userVo")
+    @ResponseBody
+    public UserVo loadUserVo(HttpSession session) {
+        UserVo userVo = new UserVo((Users) (session.getAttribute("loginUser")));
+        userVo.setKcalToday((int) historyService.totalKcalToday(userVo.getUser()));
+        userVo.setKcalOnTable((int) historyService.totalKcalOnTable(userVo.getUser()));
+        userVo.setCarbToday(Math.round(historyService.totalCarbToday(userVo.getUser()) * 100) / 100f);
+        userVo.setCarbOnTable(Math.round(historyService.totalCarbOnTable(userVo.getUser()) * 100) / 100f);
+        userVo.setPrtToday(Math.round(historyService.totalPrtToday(userVo.getUser()) * 100) / 100f);
+        userVo.setPrtOnTable(Math.round(historyService.totalPrtOnTable(userVo.getUser()) * 100) / 100f);
+        userVo.setFatToday(Math.round(historyService.totalFatToday(userVo.getUser()) * 100) / 100f);
+        userVo.setFatOnTable(Math.round(historyService.totalFatOnTable(userVo.getUser()) * 100) / 100f);
+        return userVo;
+    }
 }
--- FoodNavi/src/main/java/com/demo/persistence/ExerciseRepository.java
+++ FoodNavi/src/main/java/com/demo/persistence/ExerciseRepository.java
@@ -1,5 +1,7 @@
 package com.demo.persistence;
 
+import com.demo.domain.ExerciseOption;
+import com.demo.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.demo.domain.Exercise;
@@ -7,9 +9,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.sql.Timestamp;
+import java.time.LocalDate;
 import java.util.List;
 
 public interface ExerciseRepository extends JpaRepository<Exercise, Integer> {
+    @Query(value = "SELECT e FROM Exercise e "
+    + "WHERE e.user = :user ")
+    List<Exercise> getExerciseListByUserAndType(Users user);
 
     @Query("SELECT e FROM Exercise e WHERE e.exerciseDate >= :startDate")
     List<Exercise> findLastWeekExercises(@Param("startDate") Timestamp startDate);
